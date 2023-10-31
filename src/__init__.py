2886232ba6ffbc57d830d1eaaa9d102a3252bc76
Alejandro Arias <alejandroarias@wireless-10-155-15-102.public.utexas.edu>
2023-10-31 17:59:53 -0500

    refactored imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from .routes import user_routes
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///powerlifting.db'
+    app.register_blueprint(user_routes)
 
     db.init_app(app)
     migrate = Migrate(app, db)
-
-    from src import routes
-    routes.init_app(app)
 
     return app
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,36 @@
-from flask import current_app
+from flask import Blueprint, Response
+
+user_routes = Blueprint('user_routes', __name__)
+
+from .models import User, Exercise, Workout, db
 
 def init_app(app):
+
     @app.route('/')
     def index():
         return "Hello World"
+
+    # User routes
+    @app.route('/register', methods=['POST'])
+    def add_user():
+        app.logger("Inside add_user")
+        data = request.json
+        app.logger("Recieved request: %s", str(data))
+
+        name = data['name']
+        username = data['username']
+        password = data['password_hash']
+
+        already_registered = db.session.query(User).filter_by(name=name).first()
+        if already_registered:
+            return Response(f"User {username} already exists", status = 400)
+
+        # a newly registered worker should have no workouts, null ?
+        user = User(name=name, username=username)
+        user.set_password(password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return Response(f"User {username} successfully added", status =200)
+
